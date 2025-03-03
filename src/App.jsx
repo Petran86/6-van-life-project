5efd63642fabe277736db76eb01acd863957a1a8
Petros Kouzoupis <peterkuzu@gmail.com>
2025-03-03 13:12:49 +0200

    Additions and restructures
    Restructured header with nested routes layout, added van detail page and basic structure for host pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Vans from "./pages/Vans";
+import Vans from "./pages/Vans/Vans.jsx";
+import VanDetail from "./pages/Vans/VanDetail.jsx";
+import Dashboard from "./pages/Host/Dashboard.jsx";
+import Income from "./pages/Host/Income.jsx";
+import Reviews from "./pages/Host/Reviews.jsx";
+import Layout from "./components/Layout.jsx";
 
 import "./server.js";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">
-          #VANLIFE
-        </Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/host" element={<Dashboard />} />
+          <Route path="/host/income" element={<Income />} />
+          <Route path="/host/reviews" element={<Reviews />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
